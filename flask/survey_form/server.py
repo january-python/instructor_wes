a2315a57a54f714732324a558f361c29e7392fd6
Weston Harper <wes@tao.team>
2019-01-17 18:35:12 -0700

    added survey_form

---
+++ flask/survey_form/server.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, redirect, session
+app = Flask(__name__)
+app.secret_key = 'a;lkjsdfhdsfokweokjsdflkjawe;lkj'
+
+@app.route('/')
+def index():
+  return render_template('index.html')
+
+@app.route('/success')
+def success():
+  return render_template('success.html')
+
+@app.route('/process', methods=['POST'])
+def process():
+  session['name'] = request.form['name']
+  session['location'] = request.form['location']
+  session['favorite_language'] = request.form['favorite_language']
+  return redirect('/success')
+
+if __name__ == "__main__":
+  app.run(debug=True)
